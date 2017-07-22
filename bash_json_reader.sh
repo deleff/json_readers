b00f701a10e5841feea65bb343b579a5531f6ceb
root <root@deleff4.mylabserver.com>
2017-07-22 20:16:19 +0000

    edited bash json reader

--- bash_json_reader.sh
+++ bash_json_reader.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Make sure jq is installed
+
 data=`curl "http://maps.googleapis.com/maps/api/geocode/json?address=Belsize%20+rd,+London&sensor=false&region=uk"`
 
 server_name="address_components"
@@ -11,10 +13,9 @@ echo "The server name is \"$server_name\""
 declare -A map1
  
 while read server file ; do    
-let filenum="${#map1[@]}"+1
-echo "The path to file $filenum is $server"
-#    echo "the file name is $file"
-    map1[$server]=$file
+	map1[$server]=$file
+	echo "The path to file ${#map1[@]} is $server"
+#	echo "the file name is $file"
 
 done < <(echo "$data" | jq -r '.results[].'$server_name'[] |"\(.long_name)"') # \(.short_name)"')
 
